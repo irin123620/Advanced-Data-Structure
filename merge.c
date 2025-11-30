a53f89dd32a70a1a370d169ea7c66b8bed73eac8
Irin Sara Babu <irinsarababu@gmail.com>
2025-11-30 20:17:04 +0530

    Add files via upload

---
+++ merge.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n1, n2, i = 0, j = 0, k = 0;
+
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    int arr1[n1];
+    printf("Enter elements of first array (sorted): ");
+    for(i = 0; i < n1; i++)
+        scanf("%d", &arr1[i]);
+
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    int arr2[n2];
+    printf("Enter elements of second array (sorted): ");
+    for(i = 0; i < n2; i++)
+        scanf("%d", &arr2[i]);
+
+    int merged[n1 + n2];
+    i = j = k = 0;
+
+    while(i < n1 && j < n2)
+        merged[k++] = (arr1[i] < arr2[j]) ? arr1[i++] : arr2[j++];
+
+    while(i < n1) merged[k++] = arr1[i++];
+    while(j < n2) merged[k++] = arr2[j++];
+
+    printf("Merged array: ");
+    for(i = 0; i < n1 + n2; i++)
+        printf("%d ", merged[i]);
+    printf("\n");
+
+    return 0;
+}
